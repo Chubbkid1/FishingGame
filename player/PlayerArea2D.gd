24d10142d561350ef98fe9096f81fd434e7a580c
chubber <philip.rafting@gmail.com>
2022-07-08 20:36:25 +0200

    some cleanup and an addition of main Node

--- player/PlayerArea2D.gd
+++ player/PlayerArea2D.gd
@@ -1,26 +1,17 @@
 extends Area2D
 
-# speed is calculated in pixels per second
-#export mean that we can change the value outside of the script
+#export mean that we can change the value outside of the script aka Public
 export var speed = 150
 
 var screen_size
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
 #get viewport gives us screen size 
 func _ready():
 	screen_size = get_viewport_rect().size 
 	pass # Replace with function body.
 
-
-# func _process is made to move our character.
-# velocity is the players movement variable.
-# Clamp makes it so that the player cannot leave the screen.
-
+# this function makes our character move, pretty straight forward
+# the "clamp" funciton makes it so that the charachter cannot move outside of the screen.
 func _process(delta):
 	var velocity = Vector2.ZERO 
 	if Input.is_action_pressed("move_right"):
@@ -35,6 +26,4 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
